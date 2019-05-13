d0f15e3d96d79200911bbb3ce843fce505c96b4a
Watts <watmh@santos.com>
2019-05-14 09:07:52 +1000

    Added license header to source files and trimmed stock license

--- uc/callmanager/_abc/cmmodel.py
+++ uc/callmanager/_abc/cmmodel.py
@@ -1,6 +1,23 @@
 '''
-Call Manager Resource Abstract Base Class
-as a container
+Searches Call Manager for resources (user, jabber phone, ext. mobility profile) and reports on what is present.
+Copyright (C) 2019  Matthew Watts
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Author: wattsmj
+Contact: Please raise an issue on www.github.com/wattsmj/uc-api
+Description: Call Manager Resource Abstract Base Class as a container
 '''
 
 from abc import ABCMeta, abstractmethod
